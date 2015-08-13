71d23e3887ef5e49f81c537441de7129c8e5a6b2
Jared Jennings <jjennings@fastmail.fm>
2015-08-12 22:04:07 -0500

    fill in setup.py, first hack

--- setup.py
+++ setup.py
@@ -3,18 +3,11 @@
 from setuptools import setup
 
 setup(
-    # GETTING-STARTED: set your app name:
-    name='YourAppName',
-    # GETTING-STARTED: set your app version:
-    version='1.0',
-    # GETTING-STARTED: set your app description:
-    description='OpenShift App',
-    # GETTING-STARTED: set author name (your name):
-    author='Your Name',
-    # GETTING-STARTED: set author email (your email):
-    author_email='example@example.com',
-    # GETTING-STARTED: set author url (your url):
-    url='http://www.python.org/sigs/distutils-sig/',
-    # GETTING-STARTED: define required django version:
+    name='snowy',
+    version='0.7',
+    description='Sync server for Tomboy/Tomdroid/Conboy notes',
+    author='Jared Jennings',
+    author_email='jjennings@fastmail.fm',
+    url='https://github.com/jaredjennings/snowy',
     install_requires=['Django<=1.8'],
 )
