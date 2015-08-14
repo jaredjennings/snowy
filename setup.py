88251b91ddbbfedd96278a12db28ca3e4f499515
Jared Jennings <jjennings@fastmail.fm>
2015-08-13 23:16:53 -0500

    Revert "try a simpler ENGINE setting"
    This reverts commit 4a2e8be5d28f782d5b36ead791d4f3915081f33a.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='snowy',
-    version='0.7.1',
+    version='0.7',
     description='Sync server for Tomboy/Tomdroid/Conboy notes',
     author='Jared Jennings',
     author_email='jjennings@fastmail.fm',
